aef8e89fb969b2f8c30bbb25182f33a1861551f3
Douglas C. Rodrigues <dcorrea566@gmail.com>
2024-03-13 09:29:44 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const verdeCipreste={
+    nome:'Verde-cipreste',
+    pasta:'imagens-verde-cipreste'
+}
+
+const azulInverno={
+    nome:'Azul-inverno ',
+    pasta:'imagens-azul-inverno'
+}
+const meiaNoite={
+    nome:'Meia-noite',
+    pasta:'imagens-meia-noite'
+}
+const estelar={
+    nome:'Estelar',
+    pasta:'imagens-estelar'
+}
+const rosaClaro={
+    nome:'Rosa-claro',
+    pasta:'imagens-rosa-claro'
+}
+
+
+const imagemVisualizacao = document.getElementById('imagem-visualizacao')
+let imagemSelecionada =1;
+function trocarImagem (){
+    const idOpcaoSelecionada = document.querySelector('[name="opcao-imagem"]:checked').id.charAt(0)
+    imagemSelecionada = idOpcaoSelecionada
+    imagemVisualizacao.src = './imagens/opcoes-cores/imagens-azul-inverno/imagem-'+ imagemSelecionada +'.jpeg'
+console.log('funfou')
+
+}
